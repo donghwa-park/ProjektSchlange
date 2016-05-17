7126c849b09704fa6ae417f255ffd3d78580987d
donghwa-park <donghparke@gmail.com>
2016-05-17 21:42:20 +0200

    Add code

---
+++ Brains/RandomBrain.java
@@ -0,0 +1,27 @@
+/*
+ * Implementation of SnakeBrain which does random moves
+ * Author: Thomas Stüber
+ * */
+
+package Brains;
+
+import java.util.Random;
+
+import Logic.Field.CellType;
+import Logic.GameInfo;
+import Logic.Point;
+import Logic.Snake;
+import Logic.Snake.Direction;
+import Logic.SnakeBrain;
+
+public class RandomBrain implements SnakeBrain {
+
+	@Override
+	public Direction nextDirection(GameInfo gameInfo, Snake snake) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//todo: implement brain which does random moves
+
+}
